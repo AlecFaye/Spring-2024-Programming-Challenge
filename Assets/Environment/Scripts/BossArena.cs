80bd16a716eb397681f8228d28648ce4f8a8c0e3
AlecFaye <kelvintran.lim@outlook.com>
2024-03-24 01:31:59 -0400

    Added folders and sprites

--- Assets/Environment/Scripts/BossArena.cs
+++ Assets/Environment/Scripts/BossArena.cs
@@ -7,16 +7,22 @@ public enum Destination
     LineFireball,
     Firestorm,
     Meteor,
+
+    EntranceMushroom
 }
 
 public class BossArena : MonoBehaviour
 {
     public static BossArena Instance { get; private set; }
 
+    [Header("Fire Mage Arena Configurations")]
     [SerializeField] private Transform entranceFireMageTF;
     [SerializeField] private Transform lineFireballTF;
     [SerializeField] private Transform firestormTF;
     [SerializeField] private Transform meteorFireballTF;
+
+    [Header("Mushroom Arena Configurations")]
+    [SerializeField] private Transform entranceMushroomTF;
 
     private readonly Dictionary<Destination, Vector2> destinations = new();
 
@@ -31,6 +37,8 @@ public class BossArena : MonoBehaviour
         destinations.Add(Destination.LineFireball, lineFireballTF.position);
         destinations.Add(Destination.Firestorm, firestormTF.position);
         destinations.Add(Destination.Meteor, meteorFireballTF.position);
+
+        destinations.Add(Destination.EntranceMushroom, entranceMushroomTF.position);
     }
 
     public Vector2 GetDestination(Destination destination)
