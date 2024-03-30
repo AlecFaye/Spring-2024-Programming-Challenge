40163195da1864c1eb349a2e211666d94b439e58
AlecFaye <kelvintran.lim@outlook.com>
2024-03-30 19:53:39 -0400

    Controller input added

---
+++ Assets/UI/Scripts/InputDeviceManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum InputDevice
+{
+    KeyboardMouse,
+    Gamepad
+}
+
+public class InputDeviceManager : MonoBehaviour
+{
+    public static InputDeviceManager Instance { get; private set; }
+
+    public delegate void OnInputDeviceEvent(InputDevice inputDevice);
+    public OnInputDeviceEvent OnDeviceChanged;
+
+    public InputDevice CurrentDevice { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Debug.LogError("More than one Input Device Manager found in scene.");
+
+        Instance = this;
+    }
+
+    public void OnControlsChanged(PlayerInput playerInput)
+    {
+        CurrentDevice = (InputDevice)System.Enum.Parse(typeof(InputDevice), playerInput.currentControlScheme);
+        OnDeviceChanged?.Invoke(CurrentDevice);
+    }
+}
