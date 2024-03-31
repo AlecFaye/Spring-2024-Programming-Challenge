c47d804c6751671673168bd1dc489a6e7ed1aefa
AlecFaye <kelvintran.lim@outlook.com>
2024-03-31 04:37:00 -0400

    Fixed pause resume and cleaned up webgl

--- Assets/UI/Scripts/ResumeButton.cs
+++ Assets/UI/Scripts/ResumeButton.cs
@@ -4,10 +4,13 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class ResumeButton : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseMenuUI;
+
     private void Awake()
     {
         GetComponent<Button>().onClick.AddListener(() =>
         {
+            pauseMenuUI.SetActive(false);
             Time.timeScale = 1.0f;
         });
     }
